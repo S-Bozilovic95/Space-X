cf5784560c9716c53ee8d6c0e697f3a3f94056cc
Lacedaimon95 <svetozar95@gmail.com>
2022-09-22 13:35:48 +0200

    master/ filter syling

--- space-x/src/components/Launches/Launches.js
+++ space-x/src/components/Launches/Launches.js
@@ -2,42 +2,43 @@ import React, { useEffect, useState } from 'react';
 import API from '../../assets/apiV3';
 import LaunchesFilter from './LaunchesFilter';
 import LaunchesList from './LaunchesList';
-import {BsFilterSquare} from 'react-icons/bs';
+import { BsFilterSquare } from 'react-icons/bs';
 
 const Launches = () => {
     // states
     const [launches, setLaunches] = useState({
-        data:[],
-        filteredLaunches:[],
-        loading:true
+        data: [],
+        filteredLaunches: [],
+        loading: true
     });
-    const {data,loading,filteredLaunches} = launches;
+    const { data, loading, filteredLaunches } = launches;
 
 
-    const getAllLaunches = async()=>{
+    const getAllLaunches = async () => {
         let response = await API.get(`/launches`);
-         setLaunches({data:response.data, filteredLaunches:response.data, loading:false});
-    } 
-
-    const handleChanges = (value) =>{
-        setLaunches({...launches,filteredLaunches:value});
+        setLaunches({ data: response.data, filteredLaunches: response.data, loading: false });
     }
 
-    
+    const handleChanges = (value) => {
+        setLaunches({ ...launches, filteredLaunches: value });
+    }
 
 
-    useEffect(()=>{
+
+
+    useEffect(() => {
         getAllLaunches();
-    },[])
-    
-    
+    }, [])
 
-    return ( 
+
+
+
+    return (
         <section className='launches'>
             <LaunchesFilter launches={data} handleChanges={handleChanges} />
-             {!loading && filteredLaunches.length!=0 ? <LaunchesList launches={filteredLaunches}/>: !loading && filteredLaunches.length===0 ? "no data": "skeleton"}
+            {!loading && filteredLaunches.length != 0 ? <LaunchesList launches={filteredLaunches} /> : !loading && filteredLaunches.length === 0 ? "no data" : "skeleton"}
         </section>
-     );
+    );
 }
- 
+
 export default Launches;
--- space-x/src/components/Launches/LaunchesFilter.js
+++ space-x/src/components/Launches/LaunchesFilter.js
@@ -1,79 +1,81 @@
 import React, { useState } from 'react';
-import {BsFilterSquare} from 'react-icons/bs';
+import { BsFilterSquare } from 'react-icons/bs';
 
 
-const LaunchesFilter = ({launches,handleChanges}) => {
-    const [filter,setFilter] = useState({
-        order:false,
+const LaunchesFilter = ({ launches, handleChanges }) => {
+    const [filter, setFilter] = useState({
+        order: false,
         success: false,
-        from:0,
-        to:Math.round(new Date().getTime()/1000),
+        from: 0,
+        to: Math.round(new Date().getTime() / 1000),
     })
-    const {order,success,from,to} = filter;
+    const { order, success, from, to } = filter;
     const [showFilter, setShowFilter] = useState(false);
-    
 
 
-    const handleFilter = (e) =>{
+
+    const handleFilter = (e) => {
         e.preventDefault();
-        let arr=launches;
+        let arr = launches;
 
-        if(success === true){
-            arr = arr.filter(el=>el.launch_success === true || el.launch_success === null);
+        if (success === true) {
+            arr = arr.filter(el => el.launch_success === true || el.launch_success === null);
         }
 
-        if(order){
-            arr = arr.sort((a,b)=> b.launch_date_unix - a.launch_date_unix);
+        if (order) {
+            arr = arr.sort((a, b) => b.launch_date_unix - a.launch_date_unix);
 
-        }else if(!order){
-            arr = arr.sort((a,b)=> a.launch_date_unix - b.launch_date_unix);
+        } else if (!order) {
+            arr = arr.sort((a, b) => a.launch_date_unix - b.launch_date_unix);
         }
 
-        arr = arr.filter(el=>el.launch_date_unix >= from && el.launch_date_unix <= to);
+        arr = arr.filter(el => el.launch_date_unix >= from && el.launch_date_unix <= to);
         handleChanges(arr);
     }
 
 
 
 
-    return (  
+    return (
         <div className='launches__filter'>
-        <button type='button' className='launches__filter__funnel' onClick={()=>setShowFilter(!showFilter)}><BsFilterSquare/></button>
-        <form className={showFilter? "launches__filter__filterShow" : "launches__filter__filterHide"}  onSubmit={(e)=>handleFilter(e)}>
-            
-            <article>
-                <label>
-                    Ascending
-                    <input type="radio" name='orderBy' onChange={()=>setFilter({...filter, order:false})}/>
-                </label>
+            <button type='button' className='launches__filter__funnel' onClick={() => setShowFilter(!showFilter)}><BsFilterSquare /></button>
+            <form className={`launches__filter__box ${showFilter ? "launches__filter__filterShow" : ""}` } onSubmit={(e) => handleFilter(e)}>
 
-                <label>
-                    Descending
-                    <input type="radio" name='orderBy' onChange={()=>setFilter({...filter, order:true})}/>
-                </label>
-            </article>
+                
+                <article>
+                    <label>
+                        Ascending
+                        <input className='classicInput' type="radio" name='orderBy' onChange={() => setFilter({ ...filter, order: false })} />
+                    </label>
 
-            <article>
-                <label>
-                    From
-                    <input type="date" onChange={(e)=>setFilter({...filter, from: Math.round(new Date(e.target.value).getTime()/1000)})}/>
-                </label>
+                    <label>
+                        Descending
+                        <input className='classicInput' type="radio" name='orderBy' onChange={() => setFilter({ ...filter, order: true })} />
+                    </label>
+                
 
-                <label>
-                    To
-                    <input type="date" onChange={(e)=>setFilter({...filter, to: Math.round(new Date(e.target.value).getTime()/1000)})}/>
-                </label>
-            </article>
+                
+                    <label>
+                        From:
+                        <input className='dateInput'  type="date" onChange={(e) => setFilter({ ...filter, from: Math.round(new Date(e.target.value).getTime() / 1000) })} />
+                    </label>
 
-            <label>
-                Succesfull Launches
-                <input type="checkbox"  onChange={()=>setFilter({...filter, success:!success})}/>
-            </label>
+                    <label>
+                        To:
+                        <input className='dateInput' type="date" onChange={(e) => setFilter({ ...filter, to: Math.round(new Date(e.target.value).getTime() / 1000) })} />
+                    </label>
+                    
 
-            <input type='submit' value="apply"/>
-        </form>
+                    <label>
+                        Succesfull Launches
+                        <input className='classicInput' type="checkbox" onChange={() => setFilter({ ...filter, success: !success })} />
+                    </label>
+                </article>
+
+                <input className='submitInput' type='submit' value="apply" />
+            </form>
         </div>
-     );
+    );
 }
- 
+
 export default LaunchesFilter;
